bc4d464abd257bb4e7ce388a7227d6a811dc576a
Bruna Zunzarren <104565697+Brunazunzarrenn@users.noreply.github.com>
2022-10-06 09:39:21 -0300

    Add files via upload

---
+++ Aulas/src/br/com/Bruna/Main.java
@@ -0,0 +1,12 @@
+package br.com.Bruna;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Conta cp = new contaPoupanca();
+		cp.setSaldo(2121); 
+		cp.imprimeExtrato();
+
+	}
+
+}
